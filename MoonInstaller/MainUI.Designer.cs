d051f643427db7faa0853c4e394d09fdb47677a2
Scope <98496609+Scope03@users.noreply.github.com>
2022-05-16 23:40:56 -0400

    Fixed

--- MoonInstaller/MainUI.Designer.cs
+++ MoonInstaller/MainUI.Designer.cs
@@ -29,8 +29,16 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainUI));
             this.ColorTimer = new System.Windows.Forms.Timer(this.components);
             this.panel1 = new System.Windows.Forms.Panel();
+            this.InstallButton = new System.Windows.Forms.Button();
+            this.ZIPButton = new System.Windows.Forms.Button();
+            this.RepairButton = new System.Windows.Forms.Button();
+            this.DiscordButton = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // ColorTimer
@@ -42,16 +50,83 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(44)))), ((int)(((byte)(44)))));
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.DiscordButton);
+            this.panel1.Controls.Add(this.RepairButton);
+            this.panel1.Controls.Add(this.ZIPButton);
+            this.panel1.Controls.Add(this.InstallButton);
             this.panel1.Location = new System.Drawing.Point(3, 4);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(791, 440);
             this.panel1.TabIndex = 0;
             // 
+            // InstallButton
+            // 
+            this.InstallButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.InstallButton.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InstallButton.ForeColor = System.Drawing.Color.BlueViolet;
+            this.InstallButton.Location = new System.Drawing.Point(9, 373);
+            this.InstallButton.Name = "InstallButton";
+            this.InstallButton.Size = new System.Drawing.Size(207, 59);
+            this.InstallButton.TabIndex = 0;
+            this.InstallButton.Text = "INSTALL";
+            this.InstallButton.UseVisualStyleBackColor = true;
+            this.InstallButton.Click += new System.EventHandler(this.InstallButton_Click);
+            // 
+            // ZIPButton
+            // 
+            this.ZIPButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ZIPButton.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ZIPButton.ForeColor = System.Drawing.Color.BlueViolet;
+            this.ZIPButton.Location = new System.Drawing.Point(222, 373);
+            this.ZIPButton.Name = "ZIPButton";
+            this.ZIPButton.Size = new System.Drawing.Size(207, 59);
+            this.ZIPButton.TabIndex = 1;
+            this.ZIPButton.Text = "ZIP";
+            this.ZIPButton.UseVisualStyleBackColor = true;
+            this.ZIPButton.Click += new System.EventHandler(this.ZIPButton_Click);
+            // 
+            // RepairButton
+            // 
+            this.RepairButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.RepairButton.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RepairButton.ForeColor = System.Drawing.Color.BlueViolet;
+            this.RepairButton.Location = new System.Drawing.Point(435, 373);
+            this.RepairButton.Name = "RepairButton";
+            this.RepairButton.Size = new System.Drawing.Size(201, 59);
+            this.RepairButton.TabIndex = 2;
+            this.RepairButton.Text = "REPAIR / UNINSTALL";
+            this.RepairButton.UseVisualStyleBackColor = true;
+            this.RepairButton.Click += new System.EventHandler(this.RepairButton_Click);
+            // 
+            // DiscordButton
+            // 
+            this.DiscordButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.DiscordButton.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DiscordButton.ForeColor = System.Drawing.Color.BlueViolet;
+            this.DiscordButton.Location = new System.Drawing.Point(642, 373);
+            this.DiscordButton.Name = "DiscordButton";
+            this.DiscordButton.Size = new System.Drawing.Size(140, 59);
+            this.DiscordButton.TabIndex = 3;
+            this.DiscordButton.Text = "DISCORD";
+            this.DiscordButton.UseVisualStyleBackColor = true;
+            this.DiscordButton.Click += new System.EventHandler(this.DiscordButton_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(9, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(773, 367);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
             // MainUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Fuchsia;
+            this.BackColor = System.Drawing.Color.BlueViolet;
             this.ClientSize = new System.Drawing.Size(797, 448);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -60,6 +135,8 @@
             this.Name = "MainUI";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Moonlight Installer";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -68,6 +145,11 @@
 
         private System.Windows.Forms.Timer ColorTimer;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button InstallButton;
+        private System.Windows.Forms.Button DiscordButton;
+        private System.Windows.Forms.Button RepairButton;
+        private System.Windows.Forms.Button ZIPButton;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
--- MoonInstaller/MainUI.cs
+++ MoonInstaller/MainUI.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MelonLoader;
+using System.Diagnostics;
 
 namespace MoonInstaller
 {
@@ -21,8 +25,122 @@ namespace MoonInstaller
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            //This looks ugly lmao I'm never gonna use this
             //this.BackColor = Color.FromArgb(R.Next(0, 255), R.Next(0, 255), R.Next(0, 255));
             //this.BackColor = Color.FromArgb(R.Next(0, 255), 255, R.Next(0, 255));
+        }
+
+        private void InstallButton_Click(object sender, EventArgs e)
+        {
+            string message = "Do you want to Install the client?";
+            string title = "MOON INSTALLER";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result = MessageBox.Show(message, title, buttons);
+            if (result == DialogResult.Yes)
+            {
+                if (!File.Exists($"{MelonUtils.GameDirectory}\\Mods\\Moonlight.dll"))
+                {
+                    var wc = new WebClient();
+                    wc.DownloadFile("https://moonlightapi.000webhostapp.com/update/Moonlight.dll", $"{MelonUtils.GameDirectory}\\Mods\\Moonlight.dll");
+                }
+                else
+                {
+                    MessageBox.Show("Moonlight already exists!","MOON INSTALLER");
+                }
+
+                if (!File.Exists($"{MelonUtils.GameDirectory}\\Plugins\\MoonlightPatcher.dll"))
+                {
+                    var wc = new WebClient();
+                    wc.DownloadFile("https://moonlightapi.000webhostapp.com/update/MoonlightPatcher.dll", $"{MelonUtils.GameDirectory}\\Mods\\Moonlight.dll");
+                }
+                else
+                {
+                    MessageBox.Show("Moonlight's patcher already exists!", "MOON INSTALLER");
+                }
+
+                if (!File.Exists($"{MelonUtils.GameDirectory}\\Plugins\\MoonLoader.dll"))
+                {
+                    var wc = new WebClient();
+                    wc.DownloadFile("https://cdn.discordapp.com/attachments/950477641088118784/975964809969426462/MoonLoader.dll", $"{MelonUtils.GameDirectory}\\Mods\\Moonlight.dll");
+                }
+                else
+                {
+                    MessageBox.Show("Moonlight's patcher already exists!", "MOON INSTALLER");
+                }
+            }
+            else
+            {
+                //Just closes message box lol
+            }
+        }
+
+        private void ZIPButton_Click(object sender, EventArgs e)
+        {
+            string message = "This is for manual Installation. Are you sure you want to install it on your own? (The .ZIP file will be loaded in the 'Dropper' folder)";
+            string title = "MOON INSTALLER";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result = MessageBox.Show(message, title, buttons);
+            if (result == DialogResult.Yes)
+            {
+                var wc = new WebClient();
+                wc.DownloadFile("https://moonlightapi.000webhostapp.com/update/Client.zip", $"Dropper\\Client.zip");
+            }
+            else
+            {
+                //Nothing
+            }
+        }
+
+        private void RepairButton_Click(object sender, EventArgs e)
+        {
+            bool repair = false;
+            string message = "This process will proceed to check if Moonlight's DLLs have been changed / altered in anyway and will try its best to fix it. Do you wish to continue?";
+            string title = "MOON INSTALLER";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result = MessageBox.Show(message, title, buttons);
+            if (result == DialogResult.Yes)
+            {
+                if(repair == true)
+                {
+                    //repair shit
+                }
+
+                if (File.Exists($"{MelonUtils.GameDirectory}\\Mods\\Moonlight.dll"))
+                {
+                    File.Delete($"{MelonUtils.GameDirectory}\\Plugins\\Moonlight.dll");
+                }
+
+                if (File.Exists($"{MelonUtils.GameDirectory}\\Plugins\\MoonlightPatcher.dll"))
+                {
+                    File.Delete($"{MelonUtils.GameDirectory}\\Plugins\\MoonlightPatcher.dll");
+                }
+
+                if (File.Exists($"{MelonUtils.GameDirectory}\\Plugins\\MoonLoader.dll"))
+                {
+                    File.Delete($"{MelonUtils.GameDirectory}\\Plugins\\MoonLoader.dll");
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Process has been cancelled");
+            }
+        }
+
+        private void DiscordButton_Click(object sender, EventArgs e)
+        {
+            string message = "Do you want to be redirected to my website?";
+            string title = "MOON INSTALLER";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result = MessageBox.Show(message, title, buttons);
+            if (result == DialogResult.Yes)
+            {
+                Process.Start("https://scopes-stuff.ga/moonlight/discord.txt");
+            }
+            else
+            {
+                //Nothing
+            }
         }
     }
 }
